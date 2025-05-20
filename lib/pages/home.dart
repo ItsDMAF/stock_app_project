e044b28d21d32802f3a2f084b80bea4079a9f6bb
dmaf <dmaf.andinofiallos@gmail.com>
2025-05-21 00:29:50 +0200

    working on the stucture of the app bar and navigation bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,12 +6,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(title: Text('Counter'),),
+          //App Bar-------------------------------------------------------------
+          appBar: AppBar(title: Text('Home',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24
+            ),
+          ), shape: Border(bottom: BorderSide(color: Colors.blue, width: 3),),
+            backgroundColor: Colors.black, ),
+          //App Content Area----------------------------------------------------
           body: SafeArea(
               child: Column(
-
-              )),
+              )
+          ),
+          //Navigation Bar------------------------------------------------------
+          bottomNavigationBar: NavigationBar(destinations: [
+            NavigationDestination(icon: Icon(Icons.home, color: Colors.blue, size: 40,), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.newspaper, color: Colors.blue, size: 40,), label: 'News'),
+            NavigationDestination(icon: Icon(Icons.search, color: Colors.blue, size: 40,), label: 'Search'),
+            NavigationDestination(icon: Icon(Icons.bookmark, color: Colors.blue, size: 40,), label: 'Portfolio'),
+            NavigationDestination(icon: Icon(Icons.settings, color: Colors.blue, size: 40,), label: "Settings"),
+          ],
+            backgroundColor: Colors.black
+          ),
         )
     );
   }
