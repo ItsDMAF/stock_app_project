0aa26bc0fa728be3d62e29460255f38c78ec287a
dmaf <dmaf.andinofiallos@gmail.com>
2025-05-20 15:48:46 +0200

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('Counter'),),
+          body: SafeArea(
+              child: Column(
+
+              )),
+        )
+    );
+  }
+}
+
+
